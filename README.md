5b134f72cdefb8f0cfd6d6ab67bfb90db2ec0a4d
sangseoklim <web.tech.skp@gmail.com>
2015-01-21 13:23:38 +0900

    indentation fix

--- README.md
+++ README.md
@@ -1,36 +1,36 @@
 # CSS3D-AWatch-Look
 CSS3D based Apple Watch -like UI
 
-Major Features 
+Major Features
 - dynamically creates div for each data and calculates appropriate position and size.
-- move and rescale based on touchmove/mousemove event using CSS3D.  
+- move and rescale based on touchmove/mousemove event using CSS3D.
 
 Structure
-- main.js loads the data and attaches click events to each boxes. 
-- applewatchview.js manages mouse/touch events. When applewatchview.init() is called, or when mouse/touchmove event occurs, core.js, which is responsible for actual style calculations(scale factor and x, y, z postions), is called. 
+- main.js loads the data and attaches click events to each boxes.
+- applewatchview.js manages mouse/touch events. When applewatchview.init() is called, or when mouse/touchmove event occurs, core.js, which is responsible for actual style calculations(scale factor and x, y, z postions), is called.
 
-User controls 
+User controls
 - in main.js, options values can be changed to tune by users.
-        
+
         boxSize: boxSize, //pixel
         scaleLimitmin: 0.8,
-        scaleLimitmax: 3.0,  //range of 0.8~3.2 is suggested 
-        momentumScale: 0.4,  //bigger momentum with higher value   0.3~0.5 suggested 
+        scaleLimitmax: 3.0,  //range of 0.8~3.2 is suggested
+        momentumScale: 0.4,  //bigger momentum with higher value   0.3~0.5 suggested
         bouncedPixel: 150, //default is set equal to boxsize; in pixel
         boxMargin: 1, //space between neighboring boxes; in pixel
 
         isCircularLayout: true,
 
 - boxSize is intially set as half of window width
-- if isCircularLayout is set as true, data is postioned on the screens as circular shape. If it is set as false, data is rendered as square shape. 
+- if isCircularLayout is set as true, data is postioned on the screens as circular shape. If it is set as false, data is rendered as square shape.
 
-Coordinate caluculation Algorithms (used in core.js) 
-- for square layout: for n divs, it sets squareroot(n) number of rows. For odd index of rows, it adds half of div width for x coordinates. 
-- for circular layout: uses polar coordinate to set x and y values for each divs. For the first div, it gets it own layer. For next layers, each layer is consisted of multiple of 6 divs. For example, second layer that wraps the first div will have six divs max, third layer will have 12 divs, forth will have 18 divs, and so on. 
+Coordinate caluculation Algorithms (used in core.js)
+- for square layout: for n divs, it sets squareroot(n) number of rows. For odd index of rows, it adds half of div width for x coordinates.
+- for circular layout: uses polar coordinate to set x and y values for each divs. For the first div, it gets it own layer. For next layers, each layer is consisted of multiple of 6 divs. For example, second layer that wraps the first div will have six divs max, third layer will have 12 divs, forth will have 18 divs, and so on.
 
                 for (var n = 0; n < pts; n++) {
                         p = {};
-                        r = radius * level; 
+                        r = radius * level;
                         angleIncrement = 360 / pts;
                         p.x = r * Math.cos((angleIncrement * n) * (Math.PI / 180)) +screenCenter.centerX - radius/2;
                         p.y = r * Math.sin((angleIncrement * n) * (Math.PI / 180)) +screenCenter.centerY - radius/2;
@@ -38,13 +38,13 @@ Coordinate caluculation Algorithms (used in core.js)
                         pt += 1;
                 }
 
-- code above calculates pts number of x and y coordinates by translating polar coordinates into rectangular coordinates with cosine and sine. r is the radius of the circle, and angleIncrement is the angle spacing between two adjacent points. screenCenter - radius/2 is added in order to correctly place the divs relative to the center of the screen. 
+- code above calculates pts number of x and y coordinates by translating polar coordinates into rectangular coordinates with cosine and sine. r is the radius of the circle, and angleIncrement is the angle spacing between two adjacent points. screenCenter - radius/2 is added in order to correctly place the divs relative to the center of the screen.
 
 Issues
-- unsmooth scaling action, especially on Android devices 
+- unsmooth scaling action, especially on Android devices
 
-Project Maintainer 
+Project Maintainer
 - Jee Hyun Wang (jw3046@columbia.edu)
 
 license
-- All images are SK Planet's property. Cannot be used for commercial purposes. 
+- All images are SK Planet's property. Cannot be used for commercial purposes.
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
-* main.js 
+* main.js
 *
-* for applewatch view 
+* for applewatch view
 */
 
 var slideOn = (/android (2.|4.0|4.1)/gi).test(navigator.appVersion);
@@ -9,14 +9,14 @@ var androidOld = (/android (2.|4.0|4.1)/gi).test(navigator.appVersion);
 var inlineVideo = !(/android (2.|4.0|4.1|4.2|4.3)/gi).test(navigator.appVersion);//4.4 이상만 inline video enable
 var ios = (/iphone OS/gi).test(navigator.appVersion);
 
-var applewatch, 
-	slide,
-	launchingAnimation = !slideOn,
-	launchingDelay = 2000; //in msec
+var applewatch,
+    slide,
+    launchingAnimation = !slideOn,
+    launchingDelay = 2000; //in msec
 
 
 
-//**************** FaceCard ***********************// 
+//**************** FaceCard ***********************//
 var FaceCard = function () {};
 
 
@@ -51,42 +51,42 @@ var cubeFaceInfo = {};
 //called by getOcbData()
 function initView() {
 
-	//attach click event on each box
-	launchingDelay = 0;
+    //attach click event on each box
+    launchingDelay = 0;
     var i = 0;
-	for (key in cubeFaceInfo) {
+    for (key in cubeFaceInfo) {
 
-		obj = cubeFaceInfo[key];
-		element = document.getElementsByClassName("box")[i];
-       	i += 1;
+        obj = cubeFaceInfo[key];
+        element = document.getElementsByClassName("box")[i];
+        i += 1;
 
-       	(function() {
+        (function() {
 
-       		var landingUrl = obj.landingUrl;
-       			elm = obj.element;
-       		addEvent(element, "click", function(e) {
+            var landingUrl = obj.landingUrl;
+                elm = obj.element;
+            addEvent(element, "click", function(e) {
                 if (applewatch.started) {
                     if (this.classList.contains("youtube")) {
                         alert("동영상 시청시 데이터 네트워크 이용시 통화료가 과다하게 발생할 수 있으니 Wi-Fi로 접속하거나 전용 요금제를 이용하시기 바랍니다.");
                     } else if (landingUrl.length > 0) {
-           				if (launchingAnimation) {
-           					applewatch.launchingAnimationHandler(elm);
-           				}
-           				setTimeout( function() {
-           					window.location.href = landingUrl;
-           				}, launchingDelay);
-           			}
+                        if (launchingAnimation) {
+                            applewatch.launchingAnimationHandler(elm);
+                        }
+                        setTimeout( function() {
+                            window.location.href = landingUrl;
+                        }, launchingDelay);
+                    }
                 }
-       		});
+            });
 
-       	})();
-        
+        })();
 
-	}
 
-    //view data 
+    }
+
+    //view data
     for (key in cubeFaceInfo){
-    	obj = cubeFaceInfo[key];
+        obj = cubeFaceInfo[key];
         if (typeof obj.faceCardLoader !== "undefined") {
             console.log(obj)
             obj.faceCardLoader(obj.element, obj.iconUrl, obj.badgeUrl, obj.iframeCoverName);
@@ -101,10 +101,10 @@ function getOcbData(facecard) {
 
     $.getJSON("data/ocb.json").done(function(list) {
         var i = 0;
-        
-        //init applewatch by passing in list 
+
+        //init applewatch by passing in list
         applewatch.init(list);
-        
+
         $.each(list, function(index, data) {
 
             cubeFaceInfo[index] = {};
@@ -135,37 +135,37 @@ function getOcbData(facecard) {
 document.onload = setTimeout('init()', 50);
 
 function init(){
- 
+
 
     var facecard = new FaceCard();
 
     $(document).on("touchmove", function(e){
         e.preventDefault();
         e.stopPropagation();
-        
+
     });
 
     //scrolling on based on device orient event; only for ios
     orientationEvent = ios;
 
-    var boxSize =  (Math.min(window.innerWidth, window.innerHeight))/2.0; 
+    var boxSize =  (Math.min(window.innerWidth, window.innerHeight))/2.0;
 
     applewatch = new AppleWatchView(".viewport", {
         viewportX: -30,
         viewportY: -35,
         transDuration: "500ms",
         useTransition: true,
-        transitionThreshold: 5,//do not modify this 
+        transitionThreshold: 5,//do not modify this
         tranTimingFunc: "cubic-bezier(0.21, 0.78, 0.4, 1.02)",
         flattenDegLimit: 60,
         enableOrientationEvent: orientationEvent,
 
 
-        //default values. can be changed by options param 
+        //default values. can be changed by options param
         boxSize: boxSize, //pixel
         scaleLimitmin: 0.8,
-        scaleLimitmax: 3.0,  //range of 0.8~3.0 is suggested 
-        momentumScale: 0.4,  //bigger momentum with higher value   0.3~0.5 suggested 
+        scaleLimitmax: 3.0,  //range of 0.8~3.0 is suggested
+        momentumScale: 0.4,  //bigger momentum with higher value   0.3~0.5 suggested
         bouncedPixel: 150, //default is set equal to boxsize pixel
         boxMargin: -10, //space between neighboring boxes. pixel
 
@@ -188,84 +188,84 @@ function removeEvent (el, type, fn, capture) {
     el.removeEventListener(type, fn, !!capture);
 };
 
-var youtubePopupLoader = function(element, badgeURL){ 
- 
-    var FullScreen = "yes"; 
-    var AutoPlay = "yes"; 
-    var HighDef = "yes"; 
+var youtubePopupLoader = function(element, badgeURL){
 
-    //Calculate Page width and height 
-    var pageWidth = window.innerWidth; 
-    var pageHeight = window.innerHeight; 
+    var FullScreen = "yes";
+    var AutoPlay = "yes";
+    var HighDef = "yes";
+
+    //Calculate Page width and height
+    var pageWidth = window.innerWidth;
+    var pageHeight = window.innerHeight;
 
     var divSize = Math.min(pageWidth, pageHeight);
-    // Make Background visible... 
-    element.style.visibility = "visible"; 
+    // Make Background visible...
+    element.style.visibility = "visible";
 
-    //Create dynamic Div container for YouTube Popup Div 
-    var divobj = document.createElement('div'); 
-    divobj.setAttribute('id', badgeURL); // Set id to YouTube id 
-    divobj.className = "popup"; 
-    divobj.style.visibility = "visible"; 
-    var divWidth = divSize; 
-    var divHeight = divSize; 
-    divobj.style.width = pageWidth + "px"; 
-    divobj.style.height = pageHeight + "px"; 
+    //Create dynamic Div container for YouTube Popup Div
+    var divobj = document.createElement('div');
+    divobj.setAttribute('id', badgeURL); // Set id to YouTube id
+    divobj.className = "popup";
+    divobj.style.visibility = "visible";
+    var divWidth = divSize;
+    var divHeight = divSize;
+    divobj.style.width = pageWidth + "px";
+    divobj.style.height = pageHeight + "px";
     divobj.style.overflow = "hidden";
     divobj.addEventListener("click", function(e){
         closePopup(badgeURL);
         applewatch.started = false;
     })
-    //Set Left and top coordinates    
+    //Set Left and top coordinates
     var divLeft = (pageWidth - divWidth) /2
-    var divTop = (pageHeight - divHeight) / 2; 
+    var divTop = (pageHeight - divHeight) / 2;
 
-    divobj.style.left = "0px"; 
-    divobj.style.top = "0px"; 
+    divobj.style.left = "0px";
+    divobj.style.top = "0px";
 
-    //Create YouTube Div 
-    var youtubeDiv = document.createElement('div'); 
-    youtubeDiv.setAttribute('id', "yt" + badgeURL); 
-    youtubeDiv.className = "ytcontainer"; 
+    //Create YouTube Div
+    var youtubeDiv = document.createElement('div');
+    youtubeDiv.setAttribute('id', "yt" + badgeURL);
+    youtubeDiv.className = "ytcontainer";
     youtubeDiv.style.top = divTop + "px";
     youtubeDiv.style.left = divLeft + "px";
     youtubeDiv.style.position= "relative";
-    youtubeDiv.style.width = divSize + "px"; 
-    youtubeDiv.style.height = divSize + "px"; 
-    if (FullScreen == "yes") 
-        FullScreen="&fs=1"; 
-    else 
-        FullScreen="&fs=0"; 
-    if (AutoPlay == "yes") 
-        AutoPlay="&autoplay=1"; 
-    else 
-        AutoPlay="&autoplay=0"; 
-    if (HighDef == "yes") 
-        HighDef="&hd=1"; 
-    else 
-        HighDef="&hd=0"; 
+    youtubeDiv.style.width = divSize + "px";
+    youtubeDiv.style.height = divSize + "px";
+    if (FullScreen == "yes")
+        FullScreen="&fs=1";
+    else
+        FullScreen="&fs=0";
+    if (AutoPlay == "yes")
+        AutoPlay="&autoplay=1";
+    else
+        AutoPlay="&autoplay=0";
+    if (HighDef == "yes")
+        HighDef="&hd=1";
+    else
+        HighDef="&hd=0";
 
-    var URL = "http://www.youtube.com/v/" + badgeURL + "&hl=en&rel=0&showsearch=0" + FullScreen + AutoPlay + HighDef; 
-    var YouTube = "<object width=\"" + divWidth + "\" height=\"" + divHeight + "\">"; 
-    YouTube += "<param name=\"movie\" value=\"" + URL + "\"></param>"; 
-    YouTube += "<param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param>"; 
-    YouTube += "<embed src=\"" + URL + "\" type=\"application/x-shockwave-flash\" "; 
-    YouTube += "allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"" + divWidth + "\" height=\"" + divHeight + "\"></embed></object>"; 
-    youtubeDiv.innerHTML = YouTube; 
+    var URL = "http://www.youtube.com/v/" + badgeURL + "&hl=en&rel=0&showsearch=0" + FullScreen + AutoPlay + HighDef;
+    var YouTube = "<object width=\"" + divWidth + "\" height=\"" + divHeight + "\">";
+    YouTube += "<param name=\"movie\" value=\"" + URL + "\"></param>";
+    YouTube += "<param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param>";
+    YouTube += "<embed src=\"" + URL + "\" type=\"application/x-shockwave-flash\" ";
+    YouTube += "allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"" + divWidth + "\" height=\"" + divHeight + "\"></embed></object>";
+    youtubeDiv.innerHTML = YouTube;
 
-    divobj.appendChild(youtubeDiv); 
+    divobj.appendChild(youtubeDiv);
     document.body.appendChild(divobj);
     $(divobj).on("touchmove", function(e){
         e.preventDefault();
         e.stopPropagation();
     });
 
-    
-} 
 
-function closePopup(id){ 
-      var divobj = document.getElementById(id); 
-      var youtubeDiv = document.getElementById("yt" + id); 
-      divobj.removeChild(youtubeDiv); //remove YouTube Div 
-      document.body.removeChild(divobj); // remove Popup Div 
-} 
+}
+
+function closePopup(id){
+      var divobj = document.getElementById(id);
+      var youtubeDiv = document.getElementById("yt" + id);
+      divobj.removeChild(youtubeDiv); //remove YouTube Div
+      document.body.removeChild(divobj); // remove Popup Div
+}
